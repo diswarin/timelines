90200f3dd2ca36af357a07ddd8da9806037dccf9
diswarin <diswarin4848@gmail.com>
2025-01-01 17:25:23 +0700

    upgrade flutter version by Diswarin

--- lib/src/timeline_theme.dart
+++ lib/src/timeline_theme.dart
@@ -330,20 +330,32 @@ class TimelineThemeData with Diagnosticable {
   }
 
   @override
+  // int get hashCode {
+  //   // Warning: For the sanity of the reader, please make sure these properties
+  //   // are in the exact same order as in operator == and in the raw constructor
+  //   // and in the order of fields in the class and in the lerp() method.
+  //   final values = <Object>[
+  //     direction,
+  //     color,
+  //     nodePosition,
+  //     nodeItemOverlap,
+  //     indicatorPosition,
+  //     indicatorTheme,
+  //     connectorTheme,
+  //   ];
+  //   return hashList(values);
+  // }
+
   int get hashCode {
-    // Warning: For the sanity of the reader, please make sure these properties
-    // are in the exact same order as in operator == and in the raw constructor
-    // and in the order of fields in the class and in the lerp() method.
-    final values = <Object>[
+    return Object.hashAll([
       direction,
       color,
       nodePosition,
       nodeItemOverlap,
       indicatorPosition,
       indicatorTheme,
-      connectorTheme,
-    ];
-    return hashList(values);
+      connectorTheme]
+    );
   }
 
   @override
